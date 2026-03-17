a2522e2c76bec00556eb64a205ee86c4d483a8a7
Alan Guevara <codexalg@gmail.com>
2026-03-17 01:29:01 -0700

    Revert "Feature/splash screen UI (#2)"
    This reverts commit b365f7d73cc2c85b3f08769274922fb4239b47d0.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
-import 'core/router/app_router.dart';
 
 void main() {
   runApp(const ProviderScope(child: IncideApp()));
@@ -12,11 +11,6 @@ class IncideApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'INCIDE',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      routerConfig: AppRouter.router,
     return MaterialApp(
       title: 'INCIDE',
       debugShowCheckedModeBanner: false,

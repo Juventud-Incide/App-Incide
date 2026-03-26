6a4db5d7153ed0a8c91674d4b1e22bad999b9d89
germanLopMtz <lopezgermanedgardo@gmail.com>
2026-03-26 14:50:42 -0700

    feat(auth): add login DTO and auth response DTO

---
+++ backend/backend/Domain/OutPutDTOs/AuthOutPutDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace backend.Domain.OutPutDTOs
+{
+    public class AuthOutPutDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string UserRole { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+    }
+}

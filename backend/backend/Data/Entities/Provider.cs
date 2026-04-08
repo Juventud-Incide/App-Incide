9164d95a0080964cdbd6358bcb159e257d319387
germanLopMtz <lopezgermanedgardo@gmail.com>
2026-04-07 19:41:33 -0700

    feat(provider): add AffiliationRejectionReason field

--- backend/backend/Data/Entities/Provider.cs
+++ backend/backend/Data/Entities/Provider.cs
@@ -10,5 +10,6 @@ namespace backend.Data.Entities
         public ProviderStatus Status { get; set; } = ProviderStatus.Registered;
         public DateTime? InterviewDate { get; set; }
         public string? InterviewRejectionReason { get; set; }
+        public string? AffiliationRejectionReason { get; set; }
     }
 }

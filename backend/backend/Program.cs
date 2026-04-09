2384955733002d6ef34d647b6073754ae67dd0ba
germanLopMtz <lopezgermanedgardo@gmail.com>
2026-04-09 15:48:44 -0700

    chore(db): swap EF provider to Npgsql

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -13,7 +13,9 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("Default"),
+        npgsql => npgsql.UseNetTopologySuite()));
 
 builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<IJwtService, JwtService>();

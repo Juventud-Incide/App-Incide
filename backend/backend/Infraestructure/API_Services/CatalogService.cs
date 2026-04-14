e4d874de6fd342aae89e67429fa4d194604a4726
germanLopMtz <lopezgermanedgardo@gmail.com>
2026-04-14 08:40:23 -0700

    feat: add service item CRUD to CatalogService

--- backend/backend/Infraestructure/API_Services/CatalogService.cs
+++ backend/backend/Infraestructure/API_Services/CatalogService.cs
@@ -61,8 +61,6 @@ namespace backend.Infraestructure.API_Services
                 .Take(10)
                 .ToListAsync(ct);
         }
-<<<<<<< HEAD
-
         private static ServiceItemOutputDTO ToServiceDTO(ServiceItem s) => new()
         {
             Id           = s.Id,

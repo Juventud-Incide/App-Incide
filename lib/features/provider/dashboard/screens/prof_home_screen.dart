cf457dfce432e66e40b9a07686fc4747d3ffcd4b
SerevrGG <angel_apaez@hotmail.com>
2026-04-14 20:29:34 -0700

    fix(ui): Solución de desbordamiento horizontal en pantalla de Home

--- lib/features/provider/dashboard/screens/prof_home_screen.dart
+++ lib/features/provider/dashboard/screens/prof_home_screen.dart
@@ -330,12 +330,14 @@ class _ProfHomeScreenState extends State<ProfHomeScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
-          const Text(
-            AppStrings.opportunitiesTitle,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w900,
-              color: AppColors.textDark,
+          Expanded(
+            child: const Text(
+              AppStrings.opportunitiesTitle,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+                color: AppColors.textDark,
+              ),
             ),
           ),
           GestureDetector(

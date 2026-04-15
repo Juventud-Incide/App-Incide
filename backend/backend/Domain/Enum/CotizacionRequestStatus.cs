9c3db535dd4ede85f36592caa99729bcec043d8a
germanLopMtz <lopezgermanedgardo@gmail.com>
2026-04-15 10:23:43 -0700

    feat(cotizaciones): add CotizacionType, CotizacionRequestStatus and CotizacionStatus enums

---
+++ backend/backend/Domain/Enum/CotizacionRequestStatus.cs
@@ -0,0 +1,10 @@
+namespace backend.Domain.Enum
+{
+    public enum CotizacionRequestStatus
+    {
+        Active    = 0,
+        Assigned  = 1,
+        Completed = 2,
+        Cancelled = 3
+    }
+}

bb7b657b2e8e3cbb4b83c08f5c5e8583e59410d0
SerevrGG <angel_apaez@hotmail.com>
2026-04-17 12:02:16 -0700

    refactor(auth): Separado la logica de mock de la logica de autenticacion.

--- lib/features/auth/providers/auth_provider.dart
+++ lib/features/auth/providers/auth_provider.dart
@@ -1,4 +1,5 @@
 import 'package:app_incide/core/network/api_client.dart';
+import 'package:app_incide/features/auth/data/repositories/mock_auth_repository.dart';
 import 'package:app_incide/features/auth/domain/repositories/auth_repository.dart';
 import 'package:app_incide/features/auth/domain/repositories/network_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -72,63 +73,6 @@ final authRepositoryProvider = Provider<AuthRepository>((ref) {
   }
 });
 
-/// Capa de acceso a datos para la autenticación (Patrón Repositorio).
-///
-/// Aísla la lógica de red (API/Firebase) del manejador de estado.
-/// Actualmente utiliza datos en duro para simular respuestas del servidor
-/// y permitir el desarrollo Frontend sin bloqueos.
-class MockAuthRepository implements AuthRepository {
-  // Ahora pedimos el rol intentado para simular la separación de apps
-  /// Ejecuta la petición HTTP de inicio de sesión.
-  Future<Map<String, dynamic>> login(
-    String email,
-    String password,
-    String requestedRole,
-  ) async {
-    // TODO: (BACKEND) - Reemplazar la simulación con el SDK de Firebase Auth o API PaaS.
-    await Future.delayed(const Duration(seconds: 2));
-
-    // Casos de prueba:
-
-    // --- CREDENCIAL EXCLUSIVA PARA CLIENTES ---
-    if (email == 'cliente@correo.com' && password == 'cliente123') {
-      return {
-        'token': 'mock_token_cliente_123',
-        'role': 'cliente',
-        'status': 'aceptado',
-      };
-    }
-    // --- CREDENCIALES GENERALES / PROFESIONISTAS ---
-    else if (email == 'admin@correo.com' && password == '12345678') {
-      return {
-        'token': 'mock_token_admin_999',
-        'role': 'admin',
-        'status': 'aceptado',
-      };
-    } else if (email == 'cliente@correo.com' && password == '12345678') {
-      return {
-        'token': 'mock_token_cliente_456',
-        'role': 'cliente',
-        'status': 'aceptado',
-      }; // Cuenta de prueba para el cliente
-    } else if (email == 'espera@correo.com') {
-      return {
-        'token': 'mock_token_espera_777',
-        'role': requestedRole,
-        'status': 'pendiente',
-      };
-    } else if (email == 'rechazado@correo.com') {
-      return {
-        'token': 'mock_token_rechazado_000',
-        'role': requestedRole,
-        'status': 'rechazado',
-      };
-    } else {
-      throw Exception('Correo o contraseña incorrectos');
-    }
-  }
-}
-
 // ==========================================
 // 3. EL CONTROLADOR DE ESTADO
 // ==========================================
@@ -184,21 +128,25 @@ class AuthController extends Notifier<AuthState> {
 
     try {
       final repository = ref.read(authRepositoryProvider);
-      final resultStatus = await repository.login(email, password, role);
+
+      final responseData = await repository.login(email, password, role);
+
+      final String realToken = responseData['token'];
+      final String serverRole = responseData['role'];
+      final String userStatus = responseData['status'];
 
       // --- INTEGRACIÓN LOCAL SHAREDPREFERENCES ---
-      final token = 'dummy_token_${DateTime.now().millisecondsSinceEpoch}';
       final prefs = await SharedPreferences.getInstance();
-      await prefs.setString(AppKeys.token, token);
-      await prefs.setString(AppKeys.role, role);
-      await prefs.setString(AppKeys.profileStatus, resultStatus['status']);
+      await prefs.setString(AppKeys.token, realToken);
+      await prefs.setString(AppKeys.role, serverRole);
+      await prefs.setString(AppKeys.profileStatus, userStatus);
 
       // Actualizamos el estado de memoria global (Riverpod)
       state = state.copyWith(
         isLoading: false,
         isAuthenticated: true,
-        role: role,
-        profileStatus: resultStatus['status'],
+        role: serverRole,
+        profileStatus: userStatus,
       );
     } catch (e) {
       state = state.copyWith(isLoading: false);

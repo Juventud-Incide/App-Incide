04817677dddfbe1a744f9dbd1da804c933c18f64
SerevrGG <angel_apaez@hotmail.com>
2026-05-04 14:08:52 -0700

    chore: Añadir localización a español en la app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'core/theme/app_theme.dart';
 import 'core/router/app_router.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('es_MX', null);
   runApp(const ProviderScope(child: IncideApp()));
 }
 
@@ -16,6 +20,16 @@ class IncideApp extends ConsumerWidget {
 
     return MaterialApp.router(
       title: 'INCIDE',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('es', 'MX'), // Español de México
+        Locale('en', 'US'), // Inglés como respaldo
+      ],
+
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       routerConfig: router,
